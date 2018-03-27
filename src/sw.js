f54ceb6db47e0029023a68d8e5ea43bfbb7af1c4
Unknown <slqhoeks@gmail.com>
2018-03-27 11:19:04 +0200

    update

--- src/sw.js
+++ src/sw.js
@@ -1,12 +1,9 @@
 
 self.addEventListener('install', function(event) {
-  caches.open('bs-v1-core')
-    .then(cache => cache.add('/offline'))
-  event.waitUntil(self.skipWaiting());
+  event.waitUntil(
+    caches.open('bs-v1-core')
+      .then(cache => cache.add('/offline'))
+      .then(self.skipWaiting())
+  );
 });
 
-/*self.addEventListener('install', event => event.waitUntil(self.skipWaiting()));*/
-
-self.addEventListener('fetch', event => {
-    //event.respondWith(new Response('hijacked directly!'));
-});
