00c411ff7078279a26b270424ae04d60fb88c94e
Unknown <slqhoeks@gmail.com>
2018-03-27 11:16:58 +0200

    update

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,11 @@
-self.addEventListener('install', event => event.waitUntil(self.skipWaiting()));
+
+self.addEventListener('install', function(event) {
+  caches.open('bs-v1-core')
+    .then(cache => cache.add('/offline'))
+  event.waitUntil(self.skipWaiting());
+});
+
+/*self.addEventListener('install', event => event.waitUntil(self.skipWaiting()));*/
 
 self.addEventListener('fetch', event => {
     //event.respondWith(new Response('hijacked directly!'));
